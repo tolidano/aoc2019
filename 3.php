37a805fe869a9ae14bcb73a8f9a43b02fa32ebb0
Shawn Tolidano <shawn.tolidano@onepeloton.com>
2019-12-03 13:12:37 -0500

    3.1 working

--- 3.php
+++ 3.php
@@ -4,29 +4,28 @@ $wires = explode("\n", $data);
 $wire1 = explode(',', trim($wires[0]));
 $wire2 = explode(',', trim($wires[1]));
 
-$bounds1 = findBounds($wire1);
-var_dump($bounds1);
-$bounds2 = findBounds($wire2);
-var_dump($bounds2);
+$list1 = listPoints($wire1);
+$list2 = listPoints($wire2);
 
-$numRows = $bounds1['rows'] > $bounds2['rows'] ? $bounds1['rows'] : $bounds2['rows'];
-$numCols = $bounds1['cols'] > $bounds2['cols'] ? $bounds1['cols'] : $bounds2['cols'];
+$minDist = 1000000;
 
-for ($i = 0; $i < $numRows + 5; $i++) {
-    $rows[] = [];
+foreach ($list1 as $k => $v) {
+    if (isset($list2[$k])) {
+        echo "BOTH LINES CROSS AT $k\n";
+        $parts = explode('|', $k);
+        $rows = (int)$parts[1];
+        $cols = (int)$parts[2];
+        $dist = abs($rows) + abs($cols);
+        if ($dist < $minDist) {
+            $minDist = $dist;
+        }
+    }
 }
-$row = 5000;
-$col = 5000;
-$rows[$row][$col] = 9;
-die;
-paintWire($wire1, $rows, '1', $row, $col);
-paintWire($wire2, $rows, '2', $row, $col);
 
-function findBounds($wire) {
-    $maxRow = -1000000;
-    $minRow = 1000000;
-    $maxCol = -1000000;
-    $minCol = 1000000;
+echo "MIN: $minDist\n";
+
+function listPoints($wire) {
+    $list = [];
     $row = 0;
     $col = 0;
     foreach ($wire as $m) {
@@ -34,88 +33,30 @@ function findBounds($wire) {
         $len = (int)substr($m, 1);
         switch ($dir) {
             case 'U':
-                $row -= $len;
-                if ($row < $minRow) {
-                    $minRow = $row;
-                }
-                break;
-            case 'D':
-                $row += $len;
-                if ($row > $maxRow) {
-                    $maxRow = $row;
-                }
-                break;
-            case 'L':
-                $col -= $len;
-                if ($col < $minCol) {
-                    $minCol = $col;
-                }
-                break;
-            case 'R':
-                $col += $len;
-                if ($col > $maxCol) {
-                    $maxCol = $col;
-                }
-                break;
-        }
-    }
-    return ['rows' => $maxRow - $minRow, 'cols' => $maxCol - $minCol, 'offsetRow' => $minRow, 'offsetCol' => $minCol];
-}
-
-function paintWire($wire, &$rows, $paint, $row, $col) {
-    foreach ($wire as $m) {
-        $dir = substr($m, 0, 1);
-        $len = (int)substr($m, 1);
-        print("AN ITEM: $m $dir $len\n");
-        switch ($dir) {
-            case 'U':
                 for ($q = 0; $q < $len; $q++) {
                     $row--;
-                    if ($rows[$row][$col] != '.') {
-                        $paint = '3';
-                    }
-                    if ($row < 0 || $col < 0) {
-                        die('tried to exceed the bounds of the board going up');
-                    }
-                    $rows[$row][$col] = $paint;
+                    $list["RC|{$row}|{$col}"] = 1;
                 }
                 break;
             case 'D':
                 for ($q = 0; $q < $len; $q++) {
                     $row++;
-                    if ($rows[$row][$col] != '.') {
-                        $paint = 3;
-                    }
-                    if ($row < 0 || $col < 0) {
-                        die('tried to exceed the bounds of the board going down');
-                    }
-                    $rows[$row][$col] = $paint;
+                    $list["RC|{$row}|{$col}"] = 1;
                 }
                 break;
             case 'L':
                 for ($q = 0; $q < $len; $q++) {
                     $col--;
-                    if ($rows[$row][$col] != '.') {
-                        $paint = 3;
-                    }
-                    if ($row < 0 || $col < 0) {
-                        die('tried to exceed the bounds of the board going left');
-                    }
-                    $rows[$row][$col] = $paint;
+                    $list["RC|{$row}|{$col}"] = 1;
                 }
                 break;
             case 'R':
                 for ($q = 0; $q < $len; $q++) {
                     $col++;
-                    if ($rows[$row][$col] != '.') {
-                        $paint = 3;
-                    }
-                    if ($row < 0 || $col < 0) {
-                        die('tried to exceed the bounds of the board going right');
-                    }
-                    $rows[$row][$col] = $paint;
+                    $list["RC|{$row}|{$col}"] = 1;
                 }
                 break;
         } 
     }
+    return $list;
 }
