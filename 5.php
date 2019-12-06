cb801283948a5b3de680b6e67626fc8547bbb302
Shawn Tolidano <shawn@tolidano.com>
2019-12-05 23:59:57 -0500

    Formatting

--- 5.php
+++ 5.php
@@ -4,6 +4,26 @@ const ADD = 1;
 const MUL = 2;
 const STORE = 3;
 const OUTPUT = 4;
+const JIT = 5;
+const JIF = 6;
+const LT = 7;
+const EQ = 8;
+const GT = 9;
+const NEQ = 10;
+
+const SIZES = [
+    STOP => 1,
+    ADD => 4,
+    MUL => 4,
+    STORE => 2,
+    OUTPUT => 2,
+    JIT => 3,
+    JIF => 3,
+    LT => 4,
+    EQ => 4,
+    GT => 4,
+    NEQ => 4,
+];
 
 function restore() {
     $dump = trim(file_get_contents('5.input.txt'));
@@ -25,73 +45,67 @@ function computer($ram, $start = 0, $inputs = []) {
             $op = (int)$fop;
             $modes = '';
         } else {
-            $op = substr($digits, -2);
+            $op = (int)substr($digits, -2);
             $modes = substr($digits, 0, strlen($digits) - 2);
         }
+        $newPos = -1;
+        $jump = SIZES[$op];
+        if ($jump > 1) {
+            $val1 = (strlen($modes) > 0 && substr($modes, -1, 1) == '1') ? $ram[$pos + 1] : $ram[$ram[$pos + 1]];
+        }
+        if ($jump > 2) {
+            $val2 = (strlen($modes) > 1 && substr($modes, -2, 1) == '1') ? $ram[$pos + 2] : $ram[$ram[$pos + 2]];
+        }
+        
         switch ($op) {
             case ADD:
-                if ($modes && substr($modes, -1, 1) == '1') {
-                    $val1 = $ram[$pos + 1];
-                    $i1l = -1;
-                } else {
-                    $i1l = $ram[$pos + 1];
-                    $val1 = $ram[$i1l];
-                }
-                if (strlen($modes) > 1 && substr($modes, -2, 1) == '1') {
-                    $val2 = $ram[$pos + 2];
-                    $i2l = -1;
-                } else {
-                    $i2l = $ram[$pos + 2];
-                    $val2 = $ram[$i2l];
-                }
                 $o1l = $ram[$pos + 3];
-                $jump = 4;
                 $ram[$o1l] = $val1 + $val2;
                 break;
             case MUL:
-                if ($modes && substr($modes, -1, 1) == '1') {
-                    $val1 = $ram[$pos + 1];
-                } else {
-                    $i1l = $ram[$pos + 1];
-                    $val1 = $ram[$i1l];
-                }
-                if (strlen($modes) > 1 && substr($modes, -2, 1) == '1') {
-                    $val2 = $ram[$pos + 2];
-                } else {
-                    $i2l = $ram[$pos + 2];
-                    $val2 = $ram[$i2l];
-                }
                 $o1l = $ram[$pos + 3];
-                $jump = 4;
                 $ram[$o1l] = $val1 * $val2;
                 break;
             case STORE:
                 $o1l = $ram[$pos +1];
-                $jump = 2;
                 $ram[$o1l] = $inputs[$inputCount];
                 $inputCount++;
                 break;
             case OUTPUT:
-                if ($modes && substr($modes, -1, 1) == '1') {
-                    $o1l = -1;
-                    $val = $ram[$pos + 1];
-                } else {
-                    $o1l = $ram[$pos + 1];
-                    $val = $ram[$o1l];
-                }
-                $jump = 2;
-                echo "$val\n";
+                echo "$val1\n";
+                break;
+            case JIT:
+                $newPos = ($val1 != 0 ? $val2 : $newPos);
+                break;
+            case JIF:
+                $newPos = ($val1 == 0 ? $val2 : $newPos);
+                break;
+            case LT:
+                $o1l = $ram[$pos + 3];
+                $ram[$o1l] = ($val1 < $val2 ? 1 : 0);
+                break;
+            case GT:
+                $o1l = $ram[$pos + 3];
+                $ram[$o1l] = ($val1 > $val2 ? 1 : 0);
+                break;
+            case EQ:
+                $o1l = $ram[$pos + 3];
+                $ram[$o1l] = ($val1 == $val2 ? 1 : 0);
+                break;
+            case NEQ:
+                $o1l = $ram[$pos + 3];
+                $ram[$o1l] = ($val1 != $val2 ? 1 : 0);
                 break;
             default:
                 $err = true;
                 echo "ERROR\n";
                 $op = STOP;
         }
-        $pos += $jump;
+        $pos = ($newPos > -1) ? $newPos : $pos + $jump;
         $fop = $ram[$pos];
     }
     return $ram;
 }
 
 $ram = restore();
-$ramOut = computer($ram, 0, [1]);
+$ramOut = computer($ram, 0, [5]);
