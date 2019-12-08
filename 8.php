a4eb83285965bdbfa7f1cdbdfb144e373cd1806e
Shawn Tolidano <shawn.tolidano@onepeloton.com>
2019-12-08 00:37:19 -0500

    8.2

--- 8.php
+++ 8.php
@@ -3,18 +3,37 @@ $data = file_get_contents('8.input.txt');
 $width = 25;
 $height = 6;
 $done = false;
-$minLayer = [];
-$minZero = 100000;
+$layers = [];
 while (!$done) {
     $layer = substr($data, 0, $width * $height);
     $data = substr($data, $width * $height);
-    $cc = count_chars($layer, 1);
-    if ($cc[48] < $minZero) {
-        $minZero = $cc[48];
-        $minLayer = $cc;
-    }
+    $layers[] = $layer;
     if (strlen($data) < $width * $height) {
         $done = true;
     }
 }
-echo $minLayer[49] * $minLayer[50];
+$final = end($layers);
+for ($i = count($layers) - 2; $i >= 0; $i--) {
+    $layer = $layers[$i];
+    for ($j = 0; $j < $width * $height; $j++) {
+        if (substr($layer, $j, 1) != 2) {
+            $pixel = substr($layer, $j, 1);
+            if (substr($final, $j, 1) != substr($layer, $j, 1)) {
+                if ($j == 0) {   
+                    $final = substr($layer, $j, 1) . substr($final, 1);
+                } elseif ($j == $width * $height -1) {
+                    $final = substr($final, 0, -1) . substr($layer, $j, 1);
+                } else {
+                    $final = substr($final, 0, $j) . substr($layer, $j, 1) . substr($final, $j + 1);
+                }
+            }
+        }
+    }
+}
+printLayer($final);
+
+function printLayer($layer) {
+    for ($m = 0; $m < 6; $m++) {
+        echo str_replace(['2', '0'], ' ', substr($layer, $m * 25, 25)) . "\n";
+    }
+}
