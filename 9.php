6be39a5f6511a22cdf9cd34d30d9d595155c7e68
Shawn Tolidano <shawn@tolidano.com>
2019-12-08 16:50:06 -0500

    prep

--- 9.php
+++ 9.php
@@ -3,3 +3,13 @@ include_once('Computer.php');
 include_once('Functions.php');
 
 $l = li('9.input.txt');
+
+// l = strlen
+// p = strpos
+// s = substr
+// cc = count_chars
+// ex = explode
+// im = implode
+// c = count
+// li = lines
+// ch = chunks
