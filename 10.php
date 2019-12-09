45b4d2355c78b972ccc564937262e62089d19e1f
Shawn Tolidano <shawn@tolidano.com>
2019-12-09 00:50:26 -0500

    10 prep

---
+++ 10.php
@@ -0,0 +1,27 @@
+<?php
+include_once('Computer.php');
+include_once('Functions.php');
+include_once('Node.php');
+include_once('Tree.php');
+
+// l = strlen
+// p = strpos
+// s = substr
+// cc = count_chars
+// ex = explode
+// im = implode
+// c = count
+// li = lines
+// ch = chunks
+// t = trim
+
+/*
+$c = new Computer();
+$c->res('10.input.txt');
+$c->run([2]);
+*/
+
+/*
+$li = li('10.input.txt');
+var_dump($li);
+*/
